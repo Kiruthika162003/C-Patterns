660c6f6a75d1aa6bf5a6a7d0a18401ad7613f292
KIRUTHIKA S <89388434+Kiruthika162003@users.noreply.github.com>
2022-08-05 20:48:06 +0530

    Create R.TRIANGLE.C

---
+++ R.TRIANGLE.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int i,j,n;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<=i;j++)
+    {
+     printf("* ");
+    }
+printf("\n");
+  }
+getch();
+}
+
+output
+3
+*
+* *
+* * *
