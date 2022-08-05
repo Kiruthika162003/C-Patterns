9ab49947bc96ba979bf6786a7ce0de7b00acfc4c
KIRUTHIKA S <89388434+Kiruthika162003@users.noreply.github.com>
2022-08-05 20:43:29 +0530

    Create SQUARE OR RECTANGLE STAR PATTERN.C

---
+++ SQUARE OR RECTANGLE STAR PATTERN.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int i,j,n;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+{
+        for(j=0;j<n;j++)
+         {
+            printf("* ");
+  }
+    printf("\n");
+}
+getch();
+}
+
+
+OUTPUT
+
+3
+* * *
+* * * 
+* * *
